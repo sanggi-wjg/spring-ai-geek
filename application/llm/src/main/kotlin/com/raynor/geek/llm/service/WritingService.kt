12d463449ffd018843c15f7fd210306b4fa4d309
Raynor <jonggu.woo@fitpet.co.kr>
2025-01-19 22:42:42 +0900

    ✨ 도우미 구현

--- application/llm/src/main/kotlin/com/raynor/geek/llm/service/WritingService.kt
+++ application/llm/src/main/kotlin/com/raynor/geek/llm/service/WritingService.kt
@@ -3,7 +3,9 @@ package com.raynor.geek.llm.service
 import com.raynor.geek.llm.service.factory.OllamaOptionsFactory
 import org.slf4j.LoggerFactory
 import org.springframework.ai.chat.model.ChatResponse
+import org.springframework.ai.chat.prompt.Prompt
 import org.springframework.ai.chat.prompt.PromptTemplate
+import org.springframework.ai.chat.prompt.SystemPromptTemplate
 import org.springframework.ai.ollama.OllamaChatModel
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.Resource
@@ -14,26 +16,29 @@ import reactor.core.publisher.Flux
 class WritingService(
     private val llm: OllamaChatModel,
 ) {
-    @Value("classpath:prompts/writing-assistant-simple.st")
-    lateinit var writingAssistantSimple: Resource
+    @Value("classpath:prompts/writing/system-basic.st")
+    lateinit var systemBasicTemplate: Resource
+
+    @Value("classpath:prompts/writing/user-basic.st")
+    lateinit var userBasicTemplate: Resource
 
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     fun helpWriting(text: String): ChatResponse {
-        val prompt = PromptTemplate(
-            writingAssistantSimple,
-            mapOf("text" to text),
-        ).create(
-            OllamaOptionsFactory.exaone35WithTemperatureZero(),
+        val systemPrompt = SystemPromptTemplate(systemBasicTemplate).createMessage()
+        val userPrompt = PromptTemplate(userBasicTemplate, mapOf("text" to text)).createMessage()
+        val prompt = Prompt(
+            listOf(systemPrompt, userPrompt),
+            OllamaOptionsFactory.exaone35(),
         )
         return llm.call(prompt)
     }
 
     fun helpWritingStream(text: String): Flux<ChatResponse> {
-        val prompt = PromptTemplate(
-            writingAssistantSimple,
-            mapOf("text" to text),
-        ).create(
+        val systemPrompt = SystemPromptTemplate(systemBasicTemplate).createMessage()
+        val userPrompt = PromptTemplate(userBasicTemplate, mapOf("text" to text)).createMessage()
+        val prompt = Prompt(
+            listOf(systemPrompt, userPrompt),
             OllamaOptionsFactory.exaone35WithTemperatureZero(),
         )
         return llm.stream(prompt)

07724a0b7a45da8b397687dc0c7a9aa8f7fc93da
Raynor <jonggu.woo@fitpet.co.kr>
2025-01-22 14:43:22 +0900

    ✨코드 정리

--- llm-service/src/main/kotlin/com/raynor/geek/llmservice/service/ChattingService.kt
+++ llm-service/src/main/kotlin/com/raynor/geek/llmservice/service/ChattingService.kt
@@ -1,12 +1,13 @@
 package com.raynor.geek.llmservice.service
 
 import com.raynor.geek.llmservice.model.OllamaLLMArgument
-import com.raynor.geek.llmservice.service.demo.EmbedSimpleService
 import com.raynor.geek.llmservice.service.factory.OllamaOptionFactory
 import com.raynor.geek.llmservice.service.factory.PromptFactory
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
+import org.springframework.ai.chat.memory.InMemoryChatMemory
 import org.springframework.ai.chat.model.ChatResponse
 import org.springframework.ai.chat.prompt.PromptTemplate
-import org.springframework.ai.ollama.OllamaChatModel
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
@@ -14,11 +15,11 @@ import reactor.core.publisher.Flux
 
 @Service
 class ChattingService(
-    private val llm: OllamaChatModel,
-    private val embedSimpleService: EmbedSimpleService,
+    private val chatClientBuilder: ChatClient.Builder,
 ) {
     @Value("classpath:prompts/global/system-basic.st")
     lateinit var systemBasicTemplate: Resource
+
 
     fun chat(userInput: String, llmArgument: OllamaLLMArgument): ChatResponse {
         val prompt = PromptFactory.create(
@@ -26,7 +27,12 @@ class ChattingService(
             systemResource = systemBasicTemplate,
             promptTemplate = PromptTemplate(userInput),
         )
-        return llm.call(prompt)
+
+        return chatClientBuilder
+            .defaultAdvisors(
+                MessageChatMemoryAdvisor(InMemoryChatMemory(), "uniqueId", 50)
+            )
+            .build().prompt(prompt).call().chatResponse()!!
     }
 
     fun chatStream(userInput: String, llmArgument: OllamaLLMArgument): Flux<ChatResponse> {
@@ -35,6 +41,10 @@ class ChattingService(
             systemResource = systemBasicTemplate,
             promptTemplate = PromptTemplate(userInput),
         )
-        return llm.stream(prompt)
+        return chatClientBuilder
+            .defaultAdvisors(
+                MessageChatMemoryAdvisor(InMemoryChatMemory(), "uniqueId", 50)
+            )
+            .build().prompt(prompt).stream().chatResponse()
     }
 }
